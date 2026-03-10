651f9588b186755ef871434e8bb8f49a15781020
Ahmad Dangani Hussaini <ahmaddanganihussaini@gmail.com>
2026-03-10 05:01:06 +0100

    Fix: use env vars for credentials, add dotenv

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,8 @@ Django settings for Ahmad Dangani Hussaini's Portfolio
 """
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,6 +85,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ahmaddanganihussaini@gmail.com'   # ← your Gmail
-EMAIL_HOST_PASSWORD = 'ilif jiiu xzzw nieu '           # ← your 16-char App Password
-DEFAULT_FROM_EMAIL = 'ahmaddanganihussaini@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
